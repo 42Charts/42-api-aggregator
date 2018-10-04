46ed7566714494f71f5e383039c6d75e93a0d69e
Yoann FUKS <yfuks@student.42.fr>
2018-10-04 19:00:50 +0200

    Add time visualiation to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ if (dotenvConfigResult.error) {
 }
 
 module.exports = function (grunt) {
+    require('time-grunt')(grunt);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
     });
