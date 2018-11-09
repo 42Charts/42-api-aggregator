580252de4e559b0a4d78dca572ac97e2a2bc6cb6
Yoann FUKS <yfuks@student.42.fr>
2018-11-09 16:39:32 +0100

    Set default charset to utf8

--- app/functions/createDB.js
+++ app/functions/createDB.js
@@ -1,5 +1,5 @@
 const createDB = (db, callback) => {
-  db.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`, (err, result) => {
+  db.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME} character SET UTF8mb4 COLLATE utf8mb4_unicode_ci`, (err, result) => {
     if (err) {
       return callback(err);
     }
