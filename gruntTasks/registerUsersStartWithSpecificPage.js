efb135dfb367c8d3e31f5d7d8bdc1b50425acc1b
Yoann FUKS <yfuks@student.42.fr>
2018-11-20 18:50:56 +0100

    minor

--- gruntTasks/registerUsersStartWithSpecificPage.js
+++ gruntTasks/registerUsersStartWithSpecificPage.js
@@ -22,9 +22,9 @@ module.exports = (grunt) => {
   });*/
 
   grunt.task.registerTask('register-users-specific-pages', 'Fill users table', function () {
-    if (!grunt.config('pageToStart')) {
+    /*if (!grunt.config('pageToStart')) {
       return grunt.fail.fatal('Page to Start is mandatory');
-    }
+    }*/
 
     const done = this.async();
     mysql.connect((error) => {
